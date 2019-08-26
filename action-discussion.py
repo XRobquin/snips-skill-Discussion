8d0459932d466c25bea98692b13535a8a04f00a7
XRobquin <xrobquin@enseirb-matmeca.fr>
2019-08-26 14:58:38 +0200

    Update action-discussion.py

--- action-discussion.py
+++ action-discussion.py
@@ -2,15 +2,24 @@
 
 from hermes_python.hermes import Hermes
 
-INTENT_START_QUIZ = "start_lesson"
-INTENT_ANSWER = "give_answer"
-INTENT_INTERRUPT = "interrupt"
-INTENT_DOES_NOT_KNOW = "does_not_know"
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-def user_request_quiz(hermes, intent_message):     
-  sentence = "What is 5 times 9 ?"    
-  print("User is asking for a quiz")    
-  hermes.publish_continue_session(intent_message.session_id, sentence, [ INTENT_ANSWER, INTENT_INTERRUPT, INTENT_DOES_NOT_KNOW ])
+INTENT_VOYAGE = "voyage"
+INTENT_REACTION = "reaction"
+
+def discussion_voyage(hermes, intent_message):  
+  name = intent_message.slots.Proche.first().value
+  lieu = intent_message.slots.Lieu.first().value
+  action = intent_message.slots.Action.first().value
+  sentence = "Fantastique, dîtes-moi "
+  sentence += name
+  sentence += " j'ai toujours révé de visiter "
+  sentence += lieu
+  sentence += " comment était-ce ?" 
+  hermes.publish_continue_session(intent_message.session_id, sentence, [ INTENT_REACTION ])
 
 
-with Hermes(MQTT_ADDR) as h: h.subscribe_intent(INTENT_START_QUIZ, user_request_quiz)
+with Hermes(MQTT_ADDR) as h: 
+  h.subscribe_intent(INTENT_VOYAGE, discussion_voyage)
