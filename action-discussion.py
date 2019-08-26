a17b8ef7a1a878cdf8283b46aeeb38e28e4e4f50
XRobquin <xrobquin@enseirb-matmeca.fr>
2019-08-26 20:24:10 +0200

    Update action-discussion.py

--- action-discussion.py
+++ action-discussion.py
@@ -6,9 +6,6 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-INTENT_VOYAGE = "voyage"
-INTENT_REACTION = "reaction"
-
 def discussion_voyage(hermes, intent_message): 
   sentence = "Fantastique"
   
@@ -16,4 +13,4 @@ def discussion_voyage(hermes, intent_message):
 
 
 with Hermes(MQTT_ADDR) as h: 
-  h.subscribe_intents(intent_received).start()
+  h.subscribe_intents(discussion_voyage).start()
